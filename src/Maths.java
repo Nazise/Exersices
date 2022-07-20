b902a01dfca9914b1107be9c5d075c4821835cf5
Nazise <dinhappy96@gmail.com>
2022-07-20 11:35:22 +0600

    Initial commit

---
+++ src/Maths.java
@@ -0,0 +1,13 @@
+public class Maths {
+    public static void main(String[] args) {
+
+        int x = 12;
+        int y = 6;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
+}
+}
