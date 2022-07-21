afb80309fe8f97703f5605b2260cd8184b2e0800
Nazise <dinhappy96@gmail.com>
2022-07-21 17:13:52 +0600

    commit

--- src/NumberOfSteps.java
+++ src/NumberOfSteps.java
@@ -3,14 +3,9 @@ public class NumberOfSteps {
     public static void main(String[] args) {
         int[] arr = {14, 8, 123};
         for (int num : arr) {
-            System.out.println(num + " numberOfSteps = " + numberOfSteps(num));
+            System.out.println(num + " numberOfSteps = " + helper(num, 0));
         }
     }
-
-    public static int numberOfSteps(int num) {
-        return helper(num, 0);
-    }
-
     public static int helper(int num, int steps) {
         if (num == 0) return steps;
         if (num % 2 == 0) {
