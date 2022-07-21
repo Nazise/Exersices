b6e325be7999a64ab40a5ac7712e866a2666e695
Nazise <dinhappy96@gmail.com>
2022-07-21 10:41:45 +0600

    commit

--- src/PositiveNum.java
+++ src/PositiveNum.java
@@ -1,12 +1,8 @@
 public class PositiveNum {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, -1, -2, -3};
-        for (Number num : nums) {
-            System.out.println(num + " is negative " + isNegative(num));
+        int[] ints = {1, 2, 3, -1, -2, -3};
+        for (Integer i : ints) {
+            System.out.printf("%d is negative %b\n", i, i < 0);
         }
-    }
-
-    private static <T extends Number> boolean isNegative(T num) {
-        return num.doubleValue() < 0;
     }
 }
